20ed6fae7215cf05593cf170848070179fe81ef6
俊华 蔡 <1032031912@qq.com>
2020-07-29 10:47:48 +0800

    添加圆的半径与面积计算

---
+++ 02_计算圆的面积和周长/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02_计算圆的面积和周长
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, are, Perimeter;
+
+            r = 5;
+            //面积
+            are = Math.PI * r * r;
+            //周长
+            Perimeter = 2 * Math.PI * r;
+
+            Console.WriteLine("半径为{0}的圆,面积为：{1} 周长{2}", r, are, Perimeter);
+
+            Console.ReadKey();
+
+        }
+    }
+}
