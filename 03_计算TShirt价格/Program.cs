649a05d7cd2033025de6e2cef45542050d320422
俊华 蔡 <1032031912@qq.com>
2020-07-29 11:25:52 +0800

    混合自增自减运算

---
+++ 03_计算TShirt价格/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03_计算TShirt价格
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T_Shirt = 35;
+            int Trousers = 120;
+            int totalMoney = 3 * T_Shirt + 2 * Trousers;
+            Console.WriteLine(totalMoney);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04_加加混合计算/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_加加混合计算
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            //int b = a++ + ++a * 2 + --a + a++;
+
+            int E = a++;
+            Console.WriteLine("a:{0},E:{1}", a, E);
+
+            int D = ++a;
+            Console.WriteLine("a:{0},D:{1}", a, D);
+
+            int F = --a;
+            Console.WriteLine("a:{0},F:{1}", a, F);
+
+            int G = a++;
+            Console.WriteLine("a:{0},G:{1}", a, G);
+
+            int b = E + D * 2 + F + G;
+
+            Console.WriteLine("a:{0},b:{1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
