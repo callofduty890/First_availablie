83588548021af8294617183b9fc78ab6d01bd48a
俊华 蔡 <1032031912@qq.com>
2020-07-29 10:40:50 +0800

    变量_操作符_快捷方式

---
+++ 01_变量与常亮/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_变量与常亮
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int Main = 1;
+
+            string str1 = "A";
+            str1 = "B";
+
+            string str = "C" + 1;
+
+            Console.WriteLine("A" + 1);
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+    }
+}
--- 第二天_代码/Program.cs
+++ 第二天_代码/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace 第二天_代码
 {
     class Program
     {
+
+        /// <summary>
+        /// 主入口函数
+        /// </summary>
+        /// <param name="args">传入数组</param>
         static void Main(string[] args)
         {
+            #region  程序运行的过程说明简介
+            /*编辑代码:.cs文件当中写代码  [我们参与编写]  C# 语法规则 */
+            Console.WriteLine("我叫{0} 今年{1}", "教练", "16");
+            //编译代码 ->检查代码打包程序  vs
+
+            //连接代码->CLR干活 ->向系统提交并执行命令
+
+            //高级语言(人类) -> 机器语言(01)
+            #endregion
+
+            //ArrayList ar = new ArrayList();
+
+            //Ctrl+k+d 自动代码对齐
+            //int a=0;
+            //int b=0;
+            //Console.WriteLine("超人视觉!");
+            //Console.WriteLine("床前明月光");
+            //Console.WriteLine("疑是地上霜");
+            //Console.WriteLine("举头望明月");
+            //a++;
+            //b++;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
         }
     }
 }
