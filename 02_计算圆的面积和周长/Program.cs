fa4a6e90514eae4bfc989eda8925d9adff25d1de
俊华 蔡 <1032031912@qq.com>
2020-07-29 10:55:22 +0800

    添加输入半径自动转换

--- 02_计算圆的面积和周长/Program.cs
+++ 02_计算圆的面积和周长/Program.cs
@@ -11,13 +11,14 @@ namespace _02_计算圆的面积和周长
         {
             double r, are, Perimeter;
 
-            r = 5;
+            Console.WriteLine("请输入圆的半径:");
+            r = Convert.ToDouble(Console.ReadLine());
             //面积
             are = Math.PI * r * r;
             //周长
             Perimeter = 2 * Math.PI * r;
 
-            Console.WriteLine("半径为{0}的圆,面积为：{1} 周长{2}", r, are, Perimeter);
+            Console.WriteLine("半径为{0:0.00}的圆,面积为：{1:0.00} 周长{2:0.00}", r, are, Perimeter);
 
             Console.ReadKey();
 
